ad8aae93178c1c7aa92b6e9273f1b5a52e59f96b
Kashif ali <pandagame112211@gmail.com.com>
2026-08-22 10:57:54 +0500

    first

---
+++ main/src/Component/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='flex flex-col justify-center items-center px-6 md:px-16 lg:px-24 xl:px-32
+     text-white bg-[url("./src/assets/heroImage.png")] bg-no-repeat bg-center bg-cover h-screen'>
+      
+    </div>
+  )
+}
